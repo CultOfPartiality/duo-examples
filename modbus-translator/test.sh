93be7b3f675ecdc05381f38b85eeb4c7e7ea9fbb
cultofpartiality <jason@wridgway.net>
2024-07-15 19:29:28 +1000

    Update test
    closes old instance now

--- modbus-translator/test.sh
+++ modbus-translator/test.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-command="
-  /root/modbus-translator 192.168.42.1;
-"
-
+ssh root@192.168.42.1 "kill \`pidof modbus-translator\`"
 scp modbus-translator root@192.168.42.1:/root/
-
-ssh root@192.168.42.1 $command
+ssh root@192.168.42.1 "/root/modbus-translator 192.168.42.1"
